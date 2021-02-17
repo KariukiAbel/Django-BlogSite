072d1921f3ecf0e4a845b0d83c42ca1a5557a6d4
KariukiAbel <abelmuriithi@gmail.com>
2021-02-18 00:15:04 +0300

    In the settings.py file, I added the word '\templates\' to the Template DIR area to point django to the templates folder

--- djangonautic/views.py
+++ djangonautic/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse("Home page")
+    #return HttpResponse("Home page")
+    return render(request, 'homepage.html')
 
 def about(request):
-    return HttpResponse("about")
+    #return HttpResponse("about")
+    return render(request, 'about.html')
